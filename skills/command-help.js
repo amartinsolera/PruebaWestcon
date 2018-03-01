786ca2a15abeeccc404c8e4ca2221e58cdcc9489
Alejandro Martin-De La Fuente <amartin@eu.westcongrp.com>
2018-03-01 16:07:38 +0100

    clone add help

--- skills/command-help.js
+++ skills/command-help.js
@@ -8,6 +8,7 @@ module.exports = function (controller) {
         text += "\n- " + bot.enrichCommand(message, ".commons") + ": shows metadata about myself";
         text += "\n- " + bot.enrichCommand(message, "help") + ": spreads the word about my skills";
         text += "\n- " + bot.enrichCommand(message, "espacio") + ": I clone the room that you choose with the same people";
+        text += "\n- " + bot.enrichCommand(message, "add") + ": añado las personas al espacio actual usando su dirección de email, separados por “,” si hay multiples";
         bot.reply(message, text);
     });
 }
